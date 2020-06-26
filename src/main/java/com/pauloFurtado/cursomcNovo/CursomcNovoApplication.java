d9366cd5f166090f81357d9c55547f7561aa9197
Paulo Furtado <pssf.62.furtado@gmail.com>
2020-06-26 07:36:04 -0400

    "Criando operação de instanciação "

--- src/main/java/com/pauloFurtado/cursomcNovo/CursomcNovoApplication.java
+++ src/main/java/com/pauloFurtado/cursomcNovo/CursomcNovoApplication.java
@@ -1,13 +1,30 @@
 package com.pauloFurtado.cursomcNovo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pauloFurtado.cursomcNovo.Repositories.CategoriaRepository;
+import com.pauloFurtado.cursomcNovo.domain.Categoria;
+
 @SpringBootApplication
-public class CursomcNovoApplication {
+public class CursomcNovoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcNovoApplication.class, args);
+	}
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Banco");
+		categoriaRepository.saveAll	(Arrays.asList(cat1,cat2));
 	}
 
 }
