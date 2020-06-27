bdd5d9c3f37157c014eb27ece984f80c5fac7097
Paulo Furtado <pssf.62.furtado@gmail.com>
2020-06-26 23:25:48 -0400

    Produto e Associação muitos para muitos

--- src/main/java/com/pauloFurtado/cursomcNovo/CursomcNovoApplication.java
+++ src/main/java/com/pauloFurtado/cursomcNovo/CursomcNovoApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pauloFurtado.cursomcNovo.Repositories.CategoriaRepository;
+import com.pauloFurtado.cursomcNovo.Repositories.ProdutoRepository;
 import com.pauloFurtado.cursomcNovo.domain.Categoria;
+import com.pauloFurtado.cursomcNovo.domain.Produto;
 
 @SpringBootApplication
 public class CursomcNovoApplication implements CommandLineRunner{
@@ -18,13 +20,31 @@ public class CursomcNovoApplication implements CommandLineRunner{
 	}
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora", 800.00);
+		Produto p3 = new Produto(null,"Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
 		categoriaRepository.saveAll	(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
