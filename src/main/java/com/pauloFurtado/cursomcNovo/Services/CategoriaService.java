9a4a91c8909fadf81001a97f56d1160768ff4ed5
Paulo Furtado <pssf.62.furtado@gmail.com>
2020-06-30 08:27:37 -0400

    Ajustes no endPoint/Categorias/{id}

--- src/main/java/com/pauloFurtado/cursomcNovo/Services/CategoriaService.java
+++ src/main/java/com/pauloFurtado/cursomcNovo/Services/CategoriaService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pauloFurtado.cursomcNovo.Exceptions.ObjectNotFoundException;
 import com.pauloFurtado.cursomcNovo.Repositories.CategoriaRepository;
 import com.pauloFurtado.cursomcNovo.domain.Categoria;
 
@@ -13,9 +14,14 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Optional<Categoria> buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
+		return Optional.ofNullable(obj.orElseThrow());
+
 	}
+
 }
